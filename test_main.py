6d1e43707b499d47a957dec69c526698230c4f1d
yopi1838 <yopi1838@gmail.com>
2020-08-15 21:44:41 +0900

    fail_test

--- test_main.py
+++ test_main.py
@@ -27,6 +27,7 @@ def client():
 
 def test_health(client):
     response = client.get('/')
+    assert False
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
@@ -37,7 +38,7 @@ def test_auth(client):
     response = client.post('/auth', 
                            data=json.dumps(body),
                            content_type='application/json')
-
+    assert False
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
